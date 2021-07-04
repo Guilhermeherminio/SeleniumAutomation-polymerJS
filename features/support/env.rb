29ca16cf800b3c8a7d0496338b99c6f038a1b469
Guilherme Herminio Sousa <guilherme.sousa@ilia.digital>
2021-07-04 08:47:42 -0300

    building the project to the github

---
+++ features/support/env.rb
@@ -0,0 +1,17 @@
+require 'capybara/dsl'
+require 'capybara/rspec/matchers'
+require 'cucumber'
+require 'webdriver'
+require 'rspec'
+require 'rubocop'
+require 'faraday'
+require 'byebug'
+require 'site_prism'
+require 'capybara/cucumber'
+require 'capybara-screenshot/cucumber'
+require 'yaml'
+
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.default_max_wait_time = 20
+
+World(Capybara::DSL, Capybara::RSpecMatchers)
